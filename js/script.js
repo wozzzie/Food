64d46688a12dad17400e19b989caca6f020c99f8
MariaGornashevich <lightbrown9874@gmail.com>
2021-04-08 14:36:16 +0300

    created timer

--- js/script.js
+++ js/script.js
@@ -1,27 +1,31 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const tabs = document.querySelectorAll(".tabheader__item"),
+window.addEventListener("DOMContentLoaded", function () {
+  // Tabs
+
+  let tabs = document.querySelectorAll(".tabheader__item"),
     tabsContent = document.querySelectorAll(".tabcontent"),
     tabsParent = document.querySelector(".tabheader__items");
 
   function hideTabContent() {
-    tabsContent.forEach(item => {
-      item.style.display = "none";
+    tabsContent.forEach((item) => {
+      item.classList.add("hide");
+      item.classList.remove("show", "fade");
     });
 
-    tabs.forEach(item => {
+    tabs.forEach((item) => {
       item.classList.remove("tabheader__item_active");
     });
   }
 
   function showTabContent(i = 0) {
-    tabsContent[i].style.display = "block";
+    tabsContent[i].classList.add("show", "fade");
+    tabsContent[i].classList.remove("hide");
     tabs[i].classList.add("tabheader__item_active");
   }
 
   hideTabContent();
   showTabContent();
 
-  tabsParent.addEventListener("click", function(event) {
+  tabsParent.addEventListener("click", function (event) {
     const target = event.target;
     if (target && target.classList.contains("tabheader__item")) {
       tabs.forEach((item, i) => {
@@ -32,4 +36,61 @@ window.addEventListener("DOMContentLoaded", () => {
       });
     }
   });
+
+  // Timer
+
+  const deadline = "2021-04-20"; // deadline point
+
+  function getTimeRemaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date()),
+      days = Math.floor(t / (1000 * 60 * 60 * 24)),
+      seconds = Math.floor((t / 1000) % 60),
+      minutes = Math.floor((t / 1000 / 60) % 60),
+      hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    // function sets difference between deadline and current time
+    // Math. floor() function returns the largest integer less than or equal to a given number
+
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  }
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return "0" + num;
+    } else {
+      return num;
+    }
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector("#days"),
+      hours = timer.querySelector("#hours"),
+      minutes = timer.querySelector("#minutes"),
+      seconds = timer.querySelector("#seconds"), // get elemets from page
+      timeInterval = setInterval(updateClock, 1000); // update timer each second
+
+    updateClock(); // timer starts from the right point without updating after 1s
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+      // returns object from function getTimeRemaining
+      // set time remained at this second
+
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      } // if time run out, timer will stop
+    }
+  }
+  setClock(".timer", deadline);
 });
